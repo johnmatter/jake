c61c3862b4527cead75a324aeda96dc6816387b5
John Matter <matter@virginia.edu>
2021-02-04 23:32:59 -0500

    remove an unnecessary check from rib:draw()

--- lib/rib.lua
+++ lib/rib.lua
@@ -13,15 +13,5 @@ end
 
 -- draw rib, with default brightness 3
 function Rib:draw(brightness)
-    if self:is_initialized() then
-        g:led(self.x, self.y, brightness or 3)
-    end
-end
-
-function Rib:is_initialized()
-    return_value = false
-    if (self.x~=nil and self.y~=nil) then
-        return_value = true
-    end
-    return return_value
+    g:led(self.x, self.y, brightness or 3)
 end
