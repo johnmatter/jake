14895011413abae6704dda94c24b5acfb64cf463
John Matter <matter@virginia.edu>
2021-02-18 23:23:32 -0500

    add tentatively functional midi/crow/jf output

--- jake.lua
+++ jake.lua
@@ -12,9 +12,17 @@ music = require("musicutil")
 local MollyThePoly = require("molly_the_poly/lib/molly_the_poly_engine")
 engine.name = "MollyThePoly"
 
+-- output options
+output_names = {"audio", "midi", "audio + midi", "crow out 1+2", "crow ii JF"}
+
+active_notes = {}
+midi_out_device = 1
+midi_out_channel = 1
+
 -- init
 function init()
 
+    -- scale/root params infrastructure
     scale_names = {}
     for n=1,#music.SCALES do
         table.insert(scale_names, music.SCALES[n].name)
@@ -30,16 +38,42 @@ function init()
     scale_name = scale_names[scale_index]
     scale = music.generate_scale_of_length(root_note, scale_name, 24)
 
-    -- setup params
+    -- params
+    -- output
+    params:add_option("output", "output", output_names, 5)
+    params:set_action("output", set_output)
 
+    params:add{
+            type = "number",
+            id = "midi_out_device",
+            name = "midi out device",
+            min = 1, max = 4, default = 1,
+            action = function(value)
+                midi_out_device = midi.connect(value)
+            end
+    }
+
+    params:add{
+            type = "number",
+            id = "midi_out_channel",
+            name = "midi out channel",
+            min = 1, max = 16, default = 1,
+            action = function(value)
+                all_notes_off()
+                midi_out_channel = value
+            end
+    }
+
+
+    -- scale and root
     params:add_separator()
     params:add_option("scale", "scale", scale_names, 11)
     params:set_action("scale", set_scale)
-
     -- TODO: figure out why this displays the note off by one
     params:add_option("root_note", "root note", note_names, 48)
     params:set_action("root_note", set_root_note)
 
+    -- engine
     params:add_separator()
     MollyThePoly.add_params()
 
@@ -62,6 +96,8 @@ end
 -- are you a bad enough dude?
 function new_game()
     game_state = 'playing'
+
+    all_notes_off()
 
     -- make a snake
     -- TODO: randomize starting position
@@ -142,14 +178,26 @@ function move_snake()
     redraw()
 end
 
+-- output options
+function set_output(output)
+    all_notes_off()
+    if output == 4 then
+        -- TODO: change to ar
+        crow.output[2].action = "{to(5,0),to(0,0.25)}"
+    elseif output == 5 then
+        crow.ii.pullup(true)
+        crow.ii.jf.mode(1)
+    end
+end
+
 -- sequence and music stuff
 function set_scale(new_scale_name)
-  scale = music.generate_scale_of_length(root_note, new_scale_name, 24)
+    scale = music.generate_scale_of_length(root_note, new_scale_name, 24)
 end
 
 function set_root_note(new_root_note)
-  root_note = new_root_note
-  scale = music.generate_scale_of_length(new_root_note, scale_name, 24)
+    root_note = new_root_note
+    scale = music.generate_scale_of_length(new_root_note, scale_name, 24)
 end
 
 function advance_sequence()
@@ -157,7 +205,7 @@ function advance_sequence()
     local this_note = scale[snake.ribs[snake.active_step].note]
 
     -- turn off last note
-    -- TODO: add gate length somehow and possibility for notes to overlap
+    -- TODO: gate length?
     if last_note then
         note_off(last_note)
     end
@@ -168,15 +216,55 @@ function advance_sequence()
 
     -- advance counter
     snake.active_step = (snake.active_step) % (#snake.ribs) + 1
+
+    -- TODO: redraw snake on grid to show active step?
 end
 
+-- turn on note for selected output
 function note_on(note)
-    local freq = music.note_num_to_freq(note)
-    engine.noteOn(note,freq,80)
+    -- audio
+    if params:get("output") == 1 then
+        local freq = music.note_num_to_freq(note)
+        engine.noteOn(note, freq, 80)
+        table.insert(active_notes, note)
+    -- MIDI
+    elseif params:get("output") == 2 then
+        midi_out_device:note_on(note, 80, midi_out_channel)
+        table.insert(active_notes, note)
+    -- audio/MIDI
+    elseif params:get("output") == 3 then
+        local freq = music.note_num_to_freq(note)
+        engine.noteOn(note, freq, 80)
+        midi_out_device:note_on(note, 80, midi_out_channel)
+        table.insert(active_notes, note)
+    -- crow
+    elseif params:get("output") == 4 then
+        crow.output[1].volts = (note)/12
+        crow.output[2].execute()
+    -- jf
+    elseif params:get("output") == 5 then
+        crow.ii.jf.play_note((note-60)/12,5)
+    end
 end
 
+-- turn off note for MIDI and engine
 function note_off(note)
-    engine.noteOff(note)
+    -- if params:get("output") == 1 then
+    --     engine.noteOff(note)
+    -- elseif params:get("output") == 2 then
+    --     midi_out_device:note_off(note, nil, midi_out_channel)
+    -- elseif params:get("output") == 3 then
+        engine.noteOff(note)
+        midi_out_device:note_off(note, nil, midi_out_channel)
+    -- end
+end
+
+-- turn off all notes for MIDI and engine
+function all_notes_off()
+    for n,note in pairs(active_notes) do
+        note_off(note)
+    end
+    active_notes = {}
 end
 
 -- grid interaction
